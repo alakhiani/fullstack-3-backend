a8c1e6aecbd70b44d38a45dddbd8147724ca05d8
Avinash Lakhiani <alakhiani@yahoo.com>
2023-05-17 18:56:42 -0700

    Added negative test cases for when update project fails, it checks status 500 is returned, and an error object is returned.

--- src/controllers/__tests__/project.test.js
+++ src/controllers/__tests__/project.test.js
@@ -40,34 +40,67 @@ describe('When calling update project controller', () => {
             json: jest.fn().mockReturnThis(),
         }
 
+        global.console = {
+            log: jest.fn(),
+            error: jest.fn(),
+        }
+
         ProjectService.updateProject = jest.fn().mockResolvedValue(updatedProject);
     })
 
     // Can use test() or it(), they do the same thing
     test('should call ProjectService.updateProject with the id and projectData', async () => {
-        // Act
+        // ACT
         await ProjectController.updateProject(req, res);
 
-        // Assert
+        // ASSERT
         expect(ProjectService.updateProject).toHaveBeenCalledWith(id, projectData);
     })
 
     test('should call res.status with 200 status code', async () => {
-        // Act
+        // ACT
         await ProjectController.updateProject(req, res);
 
-        // Assert
+        // ASSERT
         expect(res.status).toHaveBeenCalledWith(200);
     })
 
     test('should call res.json with updated project data', async () => {
-        // Act
+        // ACT
         await ProjectController.updateProject(req, res);
 
-        // Assert
+        // ASSERT
         expect(res.json).toHaveBeenCalledWith({
             status: 'success',
             data: updatedProject
+        })
+    })
+
+    it('should call res.status with 500 status code when ProjectService.updateProject call fails', async () => {
+        // ARRANGE
+        const error = new Error();
+        ProjectService.updateProject = jest.fn().mockRejectedValue(error);
+
+        // ACT
+        await ProjectController.updateProject(req, res);
+
+        // ASSERT
+        expect(res.status).toHaveBeenCalledWith(500);
+    })
+
+    it('should call res.json with error message when ProjectService.updateProject call fails', async () => {
+        // ARRANGE
+        const error = new Error("Mock error");
+        ProjectService.updateProject = jest.fn().mockRejectedValue(error);
+
+        // ACT
+        await ProjectController.updateProject(req, res);
+
+        // ASSERT
+        expect(res.json).toHaveBeenCalledWith({
+            status: 'error',
+            message: 'Project could not be updated.',
+            details: error
         })
     })
 })
